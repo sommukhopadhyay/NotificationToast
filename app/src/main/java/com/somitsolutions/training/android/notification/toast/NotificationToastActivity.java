0ab7f79e8dcd2185e676044485b527b3aa6da96c
som <som.mukhopadhyay@gmail.com>
2015-11-09 16:11:44 +0530

    Initial commit

---
+++ app/src/main/java/com/somitsolutions/training/android/notification/toast/NotificationToastActivity.java
@@ -0,0 +1,25 @@
+package com.somitsolutions.training.android.notification.toast;
+
+import android.app.Activity;
+import android.os.Bundle;
+import android.view.View;
+import android.view.View.OnClickListener;
+import android.widget.Button;
+import android.widget.Toast;
+
+public class NotificationToastActivity extends Activity {
+	@Override
+	public void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.main);
+		
+		Button button = (Button) findViewById(R.id.toast_button);
+		button.setOnClickListener(new OnClickListener() {
+			
+			@Override
+			public void onClick(View v) {
+				Toast.makeText(getApplicationContext(), "Welcome IIESTians...", Toast.LENGTH_LONG).show();
+			}
+		});
+	}
+}
